f46ef9d0357d0687caf6ea9a53c54e2d8a3ff27d
史萊姆 <getsulin@gmail.com>
2025-08-16 18:07:59 +0800

    [Fix] error import route of image

--- src/pages/MaintenancePage/MaintenancePage.tsx
+++ src/pages/MaintenancePage/MaintenancePage.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as MathCatLogo } from "@/assets/MathCat_Full.svg";
+import { ReactComponent as MathCatLogo } from "@/assets/logo/MathCat_Full.svg";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { AlertTriangle, Clock, Settings } from "lucide-react";
 
